11a0cfedcb19a9f8edde6433e457233915ab6993
Yein Cho <yeinchomail@gmail.com>
2026-08-31 22:07:04 +0900

    Update multiple items

---
+++ GlovalUsing.cs
@@ -0,0 +1,2 @@
+global using Vortice.Direct2D1;
+global using Vortice.DirectWrite;
--- glc2d/G2TextureLoader.cs
+++ glc2d/G2TextureLoader.cs
@@ -2,39 +2,73 @@
 // Author: 3dapi (https://github.com/3dapi)
 // -------------------------------------------------------------------------------------------------------------------------------------------------------------
 
+using System.Drawing;
+using System.Drawing.Imaging;
+using Vortice.Direct2D1;
+using Vortice.Mathematics;
+
 static class G2TextureLoader
 {
-    private static IWICImagingFactory? _wicFactory;
-    private static IWICImagingFactory WicFactory
-    {
-        get
-        {
-            _wicFactory ??= new IWICImagingFactory();
-            return _wicFactory;
-        }
-    }
-
     public static ID2D1Bitmap? LoadBitmap(string filePath)
     {
-        var renderTarget = G2AppBase.Instance?.RenderTarget ?? throw new InvalidOperationException("ID2D1Bitmap::G2AppBase instance is not initialized.");
+        var renderTarget = G2AppBase.Instance?.RenderTarget
+            ?? throw new InvalidOperationException(
+                "ID2D1Bitmap::G2AppBase instance is not initialized.");
+
         if (string.IsNullOrWhiteSpace(filePath))
         {
-            throw new ArgumentException("파일 경로가 비어 있습니다.", nameof(filePath));
+            throw new ArgumentException(
+                "파일 경로가 비어 있습니다.",
+                nameof(filePath));
         }
+
         if (!File.Exists(filePath))
         {
             return null;
         }
-        using IWICBitmapDecoder decoder = WicFactory.CreateDecoderFromFileName(filePath);
-        using IWICBitmapFrameDecode frame = decoder.GetFrame(0);
-        using IWICFormatConverter converter = WicFactory.CreateFormatConverter();
-        converter.Initialize(frame, PixelFormat.Format32bppPBGRA);
-        return renderTarget.CreateBitmapFromWicBitmap(converter);
+
+        using Bitmap bitmap = new Bitmap(filePath);
+
+        Rectangle rect = new Rectangle(
+            0,
+            0,
+            bitmap.Width,
+            bitmap.Height);
+
+        BitmapData bitmapData = bitmap.LockBits(
+            rect,
+            ImageLockMode.ReadOnly,
+            PixelFormat.Format32bppPArgb);
+
+        try
+        {
+            var size = new SizeI(
+                bitmap.Width,
+                bitmap.Height);
+
+var bitmapProperties = new BitmapProperties(
+    new Vortice.DCommon.PixelFormat(
+        Vortice.DXGI.Format.B8G8R8A8_UNorm,
+        Vortice.DCommon.AlphaMode.Premultiplied),
+    96.0f,
+    96.0f);
+
+return renderTarget.CreateBitmap(
+    size,
+    bitmapData.Scan0,
+    (uint)bitmapData.Stride,
+    bitmapProperties);
+
+        }
+        finally
+        {
+            bitmap.UnlockBits(bitmapData);
+        }
     }
 
     public static void Release()
     {
-        _wicFactory?.Dispose();
-        _wicFactory = null;
+        // WIC를 사용하지 않으므로 해제할 리소스가 없습니다.
     }
 }
+
